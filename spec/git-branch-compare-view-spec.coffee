ecacbde99d654710e3222bd4e2d0adb51b7f4929
Bee <bee.wilkerson@ymail.com>
2018-05-26 09:59:14 -0700

    coffeescript is no more. sad. tear. Hi ES6, nice to meet you again.

--- spec/git-branch-compare-view-spec.coffee
+++
@@ -1,5 +0,0 @@
-GitBranchCompareView = require '../lib/git-branch-compare-view'
-
-describe "GitBranchCompareView", ->
-  it "has one valid test", ->
-    expect("life").toBe "easy"
---
+++ spec/git-branch-compare-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GitBranchCompareView from '../lib/git-branch-compare-view';
+
+describe('GitBranchCompareView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
